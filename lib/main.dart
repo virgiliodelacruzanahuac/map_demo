2ea9d6b501579a0e43db2f50bf81757aa9513a09
virgiliodelacruzanahuac <virgilio.delacruz@anahuac.mx>
2021-09-29 15:25:47 -0500

    v1

--- lib/main.dart
+++ lib/main.dart
@@ -71,7 +71,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Map'),
+        title: Text('Mapa'),
       ),
       body: FlutterMap(
         mapController: _mapController,
